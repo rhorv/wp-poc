a27fbdb46e0d81cb19a640d555deca920afd212d
Rudolf Horvath <rhorvath@starfleet.hu>
2021-08-25 16:19:02 +0100

    Added initial message publish

--- src/main/java/poc/MyApplication.java
+++ src/main/java/poc/MyApplication.java
@@ -3,6 +3,8 @@ package poc;
 import clearing.domain.command.ClearPaymentCommand;
 import events.publisher.IPublish;
 import java.util.UUID;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApplication {
 
@@ -20,6 +22,19 @@ public class MyApplication {
       System.out.println("Launching pricing application ...");
       pricing.Consumer consumer = new pricing.Consumer();
       consumer.start();
+    } else if (args[0].equals("publish")) {
+      System.out.println("Publishing initial clearing message ...");
+      ApplicationContext appContext =
+          new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+      IPublish publisher = (IPublish) appContext.getBean("clearing.messagePublisher");
+      try {
+        publisher.publish(
+            new ClearPaymentCommand(UUID.fromString(args[1]))
+        );
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     } else {
       throw new Exception("'" + args[0] + "' is not a valid app name");
     }
